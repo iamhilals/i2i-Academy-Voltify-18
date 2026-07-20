b0b9a37b277469f47bac042fa31aa5212f964d52
Hilal Ayşe Akgül <Hilalayseakgull@gmail.com>
2026-07-20 20:54:21 +0300

    chore: initial project setup with docker, spring boot and react

---
+++ voltify-core/src/main/java/com/voltify/core/VoltifyCoreApplication.java
@@ -0,0 +1,13 @@
+package com.voltify.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltifyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltifyCoreApplication.class, args);
+	}
+
+}
---
+++ voltify-core/src/test/java/com/voltify/core/VoltifyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voltify.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltifyCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
